3ed0923d49ac068d86cc7b666dcb2ecb738a2e00
Antek <antek.olszewski01@gmail.com>
2021-01-12 11:30:32 +0100

    backsubst.c i nowe dane

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -5,18 +5,28 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	int i, j;
+	double wynik;
+	if (mat->c != mat->r)
+		return 2;
+	if (mat->c != b->r)
+		return 2;
+	for (i = 0; i < x->c; i++)
+	{
+		if (mat->data[i][i] == 0)
+			return 1;
+	}
+	for (i = mat->c - 1; i >= 0; i--)
+	{
+		wynik = b->data[i][0];
+		for (j = mat->c - 1; j > i; j--)
+		{
+			wynik -= mat->data[i][j] * x->data[j][0];
+		}
+			wynik /= mat->data[i][j];
+			x->data[i][0] = wynik;
+	}
+	return 0;
 }
 
 
