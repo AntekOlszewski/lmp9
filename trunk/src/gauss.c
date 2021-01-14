03b0c146314a3975c09a67c111e9762771cca06d
aleksyniemir <aleksander.niemirka@gmail.com>
2021-01-14 11:11:04 +0100

    eliminacja

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -5,10 +5,18 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
 
-		return 0;
+
+
+	int i, j, k;
+    	for (i = 0; i < mat->r; i++) {
+            for (k = i + 1; k <= mat->r; k++) {
+		    double  term = mat->dane[k][i] / mat->dane[i][i];//a[k][i] / a[i][i];
+	            for (j = 0; j < mat->c+1; j++) {
+                	    mat->dane[k][j] = mat->dane[k][j] - term * mat->dane[i][j]; //a[k][j]=a[k][j]-term*a[i][j];
+	            }
+	            b->dane[k][0] = b->dane[k][0] - term * mat->dane[i][j];
+								        }
 }
-
+        return 0;
+}
