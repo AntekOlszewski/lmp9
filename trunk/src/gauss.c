641093d630ea593cb176a4277e598a6c01cc27b4
aleksyniemir <aleksander.niemirka@gmail.com>
2021-01-14 20:45:56 +0100

    poprawiona eliminacja

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,22 +1,25 @@
 #include "gauss.h"
-
+#include <stdio.h>
+#include "mat_io.h"
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-
-
-
 	int i, j, k;
-    	for (i = 0; i < mat->r; i++) {
-            for (k = i + 1; k <= mat->r; k++) {
-		    double  term = mat->dane[k][i] / mat->dane[i][i];//a[k][i] / a[i][i];
-	            for (j = 0; j < mat->c+1; j++) {
-                	    mat->dane[k][j] = mat->dane[k][j] - term * mat->dane[i][j]; //a[k][j]=a[k][j]-term*a[i][j];
+	double  term, s;
+	printf("pocz gausa");
+    	for (i = 0; i < mat->r -1; i++) {
+	    if ( mat->data[i][i] == 0 ) return 1;
+            for (k = i + 1; k < mat->r; k++) {
+		    term = mat->data[k][i] / mat->data[i][i];
+		    for (j = 0; j < mat->c; j++){
+                	    mat->data[k][j] = mat->data[k][j] - term * mat->data[i][j]; 
 	            }
-	            b->dane[k][0] = b->dane[k][0] - term * mat->dane[i][j];
-								        }
-}
+		    b->data[k][0] = b->data[k][0] - term * b->data[i][0];
+	    }
+	}
+	
+
         return 0;
 }
